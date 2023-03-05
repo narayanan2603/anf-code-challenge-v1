754f47efcac189909fb51d0c2ee066b093f8f902
NK <nkannan@80la-nkannan-3.zebra.lan>
2023-03-05 15:10:13 -0600

    added Annotations

--- core/src/main/java/com/anf/core/pojo/News.java
+++ core/src/main/java/com/anf/core/pojo/News.java
@@ -1,63 +1,30 @@
 package com.anf.core.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  */
 public class News {
 
+    @Getter
+    @Setter
     private String author;
+    @Getter
+    @Setter
     private String content;
+    @Getter
+    @Setter
     private String description;
+    @Getter
+    @Setter
     private String title;
+    @Getter
+    @Setter
     private String url;
+    @Getter
+    @Setter
     private String urlImage;
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUrlImage() {
-        return urlImage;
-    }
-
-    public void setUrlImage(String urlImage) {
-        this.urlImage = urlImage;
-    }
 
 }
